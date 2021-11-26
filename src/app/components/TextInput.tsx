e63744adc55e4bdfb5d7f75ac78822d22c4057e0
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-11-26 04:38:18 +0300

    Added text field checks

--- src/app/components/TextInput.tsx
+++ src/app/components/TextInput.tsx
@@ -1,4 +1,4 @@
 import "./TextInput.scss";
 import {FormEvent} from "react";
-export const TextInput = (props: {type: string, placeholder: string, onChange?: (event: FormEvent<HTMLInputElement>) => void, id?: string}) =>
-    <input className="TextInput" id={props.id} type={props.type} placeholder={props.placeholder} onChange={props.onChange} />
+export const TextInput = (props: {type: string, placeholder: string, onChange?: (event: FormEvent<HTMLInputElement>) => void, id?: string, error?: boolean}) =>
+    <input className={props.error ? "TextInputErr" : "TextInput"} id={props.id} type={props.type} placeholder={props.placeholder} onChange={props.onChange} />
--- src/app/pages/LoginPage.tsx
+++ src/app/pages/LoginPage.tsx
@@ -3,10 +3,14 @@ import {AuthContainer} from "../components/AuthContainer";
 import {FormButton} from "../components/FormButton";
 import {FormEvent, useState} from "react";
 
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+type TextField = {value: string, error?: boolean}
+
 export const LoginPage = () => {
     //const data = useState({email: "", password: ""});
-    const [email, setEmail] = useState("");
+    const [email, setEmail] = useState<TextField>({value: ""});
     const [password, setPassword] = useState("");
+    const [tooltip, setTooltip] = useState("");
 
     const onChange = (event: FormEvent<HTMLInputElement>) => {
         const target = event.target as HTMLTextAreaElement;
@@ -18,20 +22,25 @@ export const LoginPage = () => {
                 setPassword(target.value);
         }*/
         if (target.type === "email") {
-            setEmail(target.value);
+            setEmail({value: target.value});
         } else {
             setPassword(target.value);
         }
-
+        setTooltip("")
     }
     const onSubmit = (event: FormEvent<HTMLInputElement>) => {
-        console.log(event);
+        if (!emailRegex.test(email.value)) {
+            setTooltip("Введите действительный email");
+            setEmail({value: "", error: true});
+            return
+        }
     }
 
     return (
         <AuthContainer>
-            <TextInput type="email" placeholder="Адрес электронной почты" onChange={onChange} />
+            <TextInput type="email" placeholder="Адрес электронной почты" onChange={onChange} error={email.error} />
             <TextInput type="password" placeholder="Пароль" onChange={onChange} />
+            {tooltip ? <p id="tooltip">{tooltip}</p> : <></>}
             <FormButton value="Войти" disabled={!email || !password} onClick={onSubmit} />
             <p>Еще не зарегистрированы? <a href="/">Регистрация</a></p>
         </AuthContainer>
