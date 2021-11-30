4c4d05e8c042292ba6d14b344eccc4df90b87e2c
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-01 01:38:28 +0300

    Implemented avatars

--- src/app/components/AvatarButton.tsx
+++ src/app/components/AvatarButton.tsx
@@ -1,5 +1,20 @@
 import "./AvatarButton.scss";
+import {setAuthorized} from "../../App";
+import {wlAPI} from "../utils/wl_api";
+import React, {useState} from "react";
+import {Loader} from "./Loader";
+
+const onLogout = () => {
+    localStorage.removeItem("token");
+    setAuthorized(false);
+}
+
+const fetchAvatar = async () => wlAPI.userProfile(localStorage.getItem("token"))
 
 export const AvatarButton = () => {
-    return <img className="avatarImage" alt="Profile avatar" src="/logo_short.png" />
+    const [avatar, setAvatar] = useState<string | undefined>(undefined);
+    if (avatar === undefined) fetchAvatar().then(url => url && setAvatar ? setAvatar(url) : "");
+    console.log(avatar);
+    return avatar ?
+        <img className="avatarImage" alt="Profile avatar" src={avatar} onClick={onLogout} title="Click to logout" /> : <Loader />
 }
--- src/app/utils/wl_api.ts
+++ src/app/utils/wl_api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {setAuthorized} from "../../App";
 
 const instance = axios.create({
     baseURL: "https://wl.api.tyuop.tk/"
@@ -8,12 +9,28 @@ export class wlAPI {
     static login(login: string, password: string) {
         return instance.post("login",{login, password});
     }
-    static profile(token: string) {
-        return instance.get("profile",{
+    static async profile(token: string) {
+        const res = await instance.get("profile",{
             headers: {
                 authorization: token
             }
         })
+        if (res.data.success === false) {
+            console.log(res.data);
+            localStorage.removeItem("token");
+            alert(res.data.message);
+            setAuthorized(false);
+        }
+        return res
+    }
+    private static avatar: string;
+    private static avatar_promise: Promise<string>;
+    static userProfile(token: string | null) {
+        if (this.avatar) return this.avatar;
+        return this.avatar_promise ?? (this.avatar_promise = this.userProfileForced(token))
+    }
+    private static async userProfileForced(token: string | null) {
+        return this.avatar = (await this.profile(token ?? "")).data.avatar_url;
     }
 }
 
