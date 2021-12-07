6f52ffe3271f12a5b673e4d8f905b764f1cf6ed1
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-07 17:37:59 +0300

    Movie showcase page (styling mockup)

--- src/app/pages/MovieShowcasePage.tsx
+++ src/app/pages/MovieShowcasePage.tsx
@@ -3,6 +3,9 @@ import {Movie} from "../utils/types";
 import {wlAPI} from "../utils/wl_api";
 import {Loader} from "../components/Loader";
 import {useParams} from "react-router-dom";
+import "./MovieShowcasePage.scss";
+import plusIcon from "../assets/Plus.svg";
+import {Button} from "../components/Button";
 
 export const MovieShowcasePage = () => {
     const [movie, setMovie] = useState<Movie>();
@@ -10,5 +13,15 @@ export const MovieShowcasePage = () => {
     useEffect(() => {
         wlAPI.movie(parseInt(params.id ?? "-1")).then(res => setMovie(res.data))
     })
-    return movie ? <p>{JSON.stringify(movie)}</p> : <Loader/>
+    return movie ? <div className="showcase">
+        <img src={movie.thumbnail_url} alt={movie.title} />
+        <div className="description">
+            <h1>{movie.title}</h1>
+            <h6>{movie.date}</h6>
+            <h5>{movie.rating}</h5>
+            <Button id="watchlist" text="Буду смотреть" toggled={true} image={plusIcon}/>
+            <p>{movie.description}</p>
+            <b>{movie.genres.join(", ")}</b>
+        </div>
+    </div> : <Loader/>
 }
